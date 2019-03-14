dcb96b766e34711c89dfdb6054e4bfd94bbdf4fb
jasonrosendale <jason.rosendale@gmail.com>
2019-03-13 22:43:14 -0700

    Remove global variables

--- lib/industrialist/manufacturable.rb
+++ lib/industrialist/manufacturable.rb
@@ -4,17 +4,20 @@ module Industrialist
   module Manufacturable
     def self.included(base)
       base.extend ClassMethods
+      base.instance_variable_set(:@factory, Industrialist::Factory.new)
     end
 
     module ClassMethods
-      @@factory = Industrialist::Factory.new
+      def inherited(base)
+        base.instance_variable_set(:@factory, @factory)
+      end
 
       def create_factory(identifier)
-        Object.const_set(identifier, @@factory)
+        Object.const_set(identifier, @factory)
       end
 
       def corresponds_to(key)
-        @@factory.register(key, self)
+        @factory.register(key, self)
       end
     end
   end
--- spec/lib/industrialist/manufacturable_spec.rb
+++ spec/lib/industrialist/manufacturable_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Industrialist::Manufacturable do
-  before do
+  def undefine_constants(*consts)
+    consts.each { |const| Object.send(:remove_const, const) }
+  end
+
+  around do |example|
     class Automobile
       include Industrialist::Manufacturable
       create_factory :AutomobileFactory
     end
+    class Book
+      include Industrialist::Manufacturable
+      create_factory :BookFactory
+    end
+    example.run
+    undefine_constants(:Automobile, :Book, :AutomobileFactory, :BookFactory)
   end
+
 
   describe '.factory_name' do
     it 'assigns the provided name to the factory instance' do
@@ -15,14 +26,21 @@ RSpec.describe Industrialist::Manufacturable do
   end
 
   describe '.corresponds_to' do
-    before do
-      class Sedan < Automobile
-        corresponds_to :sedan
-      end
+    around do |example|
+      class Sedan     < Automobile; corresponds_to :sedan    ; end
+      class Paperback < Book      ; corresponds_to :paperback; end
+      example.run
+      undefine_constants(:Sedan, :Paperback)
     end
 
     it 'registers the class under the provided key' do
       expect(AutomobileFactory.registry[:sedan]).to equal(Sedan)
+      expect(BookFactory.registry[:paperback]).to equal(Paperback)
+    end
+
+    it 'registers the class only under the provided key' do
+      expect(AutomobileFactory.registry[:paperback]).to be_nil
+      expect(BookFactory.registry[:sedan]).to be_nil
     end
   end
 end
